71369a03b506c07abaa69671c562cafeed252caf
Lucio <luciocarvalho10@hotmail.com.br>
2021-08-30 22:30:21 -0400

    Create project and configs

---
+++ public/project.js
@@ -0,0 +1,17 @@
+let projects = [
+  {
+    name: 'project one',
+    tags: '#',
+    image: 'project01.png'
+  },
+  {
+    name: 'project two',
+    tags: '#',
+    image: 'project02.png'
+  },
+  {
+    name: 'project three',
+    tags: '#',
+    image: 'project03.png'
+  },
+]
