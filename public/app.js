798546c088024687cb26fa46d7dc611f1fb95f23
Lucio <luciocarvalho10@hotmail.com.br>
2021-09-02 19:00:38 -0400

    create mail route with contact form

--- public/app.js
+++ public/app.js
@@ -28,3 +28,26 @@ filters.forEach(f => f.addEventListener('click', () => {
   filters.forEach(btn => CLr(btn, 'active'))
   CLa(f, 'active')
 }))
+
+// contact form
+
+const contactBtn = document.querySelector('.contact-btn')
+
+contactBtn.addEventListener('click', () => {
+  const resp = {
+    firstname: document.querySelector('.first-name').value,
+    lastname: document.querySelector('.last-name').value,
+    email: document.querySelector('.email').value,
+    msg: document.querySelector('.message').value
+  }
+
+  if( resp.firstname.length && resp.lastname.length && resp.email.length && resp.msg.length ) {
+   fetch('/mail', {
+     method: 'post',
+     headers: new Headers({'Content-Type': 'application/json'}),
+     body: JSON.stringify({...resp})
+   })
+   .then(res => res.json())
+   .then(info => alert(info))
+  }
+})
--- server.js
+++ server.js
@@ -12,5 +12,32 @@ app.use(express.static(initialPath))
 app.use(express.json())
 
 app.get('/', (req, res) => {res.sendFile(path.join(initialPath, 'index.html'))})
+app.post('mail', (req, res) => {
+  const { firstname, lastname, email, msg } = req.body
+
+  const transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  })
+
+  const mailOptions = {
+    from: 'sender email',
+    to: 'receiver email',
+    subject: 'Postfolio',
+    text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, \nMessage: ${msg}`
+  }
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err)
+      res.json('Something wrong!!! Please, try again.')
+    } else {
+      res.json('Thanks for e-mailling me. I will reply to you within soon!!!')
+    }
+  })
+})
 
 app.listen(3000, () => {console.log('listen door 3000 at ' + new Date())})
